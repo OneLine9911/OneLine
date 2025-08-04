373bcfe178cbe40bf023901ae58795a4b837c335
OneLine9911 <oneline998899>
2025-08-04 23:46:41 +0200

    new pages

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,11 +1,17 @@
-import { Alert, Button, StyleSheet, Text } from 'react-native'
+import { useRouter } from 'expo-router'
+import { Alert, Button, Pressable, StyleSheet, Text, View } from 'react-native'
+import Icon from '../../assets/icons'
+import Avatar from '../../components/Avatar'
 import ScreenWrapper from '../../components/ScreenWrapper'
+import { theme } from '../../constants/theme'
 import { useAuth } from '../../contexts/AuthContext'
+import { hp, wp } from '../../helpers/common'
 import { supabase } from '../../lib/supabase'
 
 const Home = () => {
 
-    const { setAuth } = useAuth();
+    const { user, setAuth } = useAuth();
+    const router = useRouter();
 
     const onLogout = async () => {
         const { error } = await supabase.auth.signOut();
@@ -15,8 +21,29 @@ const Home = () => {
     }
 
     return (
-        <ScreenWrapper>
-            <Text>Home</Text>
+        <ScreenWrapper bg='white'>
+            <View style={styles.container}>
+                {/* header */}
+                <View style={styles.header}>
+                    <Text style={styles.title}>OneLine</Text>
+                    <View style={styles.icons}>
+                        <Pressable onPress={() => router.push('notifications')}>
+                            <Icon name="heart" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+                        </Pressable>
+                        <Pressable onPress={() => router.push('newPost')}>
+                            <Icon name="plus" size={hp(3.2)} strokeWidth={2} color={theme.colors.text} />
+                        </Pressable>
+                        <Pressable onPress={() => router.push('profile')}>
+                            <Avatar
+                                uri={user?.image}
+                                size={hp(4.3)}
+                                rounded={theme.radius.sm}
+                                style={{ borderWidth: 2 }}
+                            />
+                        </Pressable>
+                    </View>
+                </View>
+            </View>
             <Button title='logout' onPress={onLogout} />
         </ScreenWrapper>
     )
@@ -24,4 +51,60 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // paddingHorizontal: wp(4)
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+        marginHorizontal: wp(4)
+    },
+    title: {
+        color: theme.colors.text,
+        fontSize: hp(3.2),
+        fontWeight: theme.fonts.bold
+    },
+    avatarImage: {
+        height: hp(4.3),
+        width: hp(4.3),
+        borderRadius: theme.radius.sm,
+        borderCurve: 'continuous',
+        borderColor: theme.colors.gray,
+        borderWidth: 3
+    },
+    icons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 18
+    },
+    listStyle: {
+        paddingTop: 20,
+        paddingHorizontal: wp(4)
+    },
+    noPosts: {
+        fontSize: hp(2),
+        textAlign: 'center',
+        color: theme.colors.text
+    },
+    pills: {
+        position: 'absolute',
+        right: -10,
+        top: -4,
+        height: hp(2.2),
+        width: hp(2.2),
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+        backgroundColor: theme.colors.roseLight
+    },
+    pillText: {
+        color: 'white',
+        fontSize: hp(1.2),
+        fontWeight: theme.fonts.bold
+    }
+})
