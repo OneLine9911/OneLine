65b78757ea3b524a9fba804bdb00c4636a2b8926
OneLine9911 <oneline9911@gmail.com>
2025-08-07 15:51:54 +0200

    Profile

--- components/RichTextEditor.jsx
+++ components/RichTextEditor.jsx
@@ -11,7 +11,6 @@ const RichTextEditor = ({
                 editor={editorRef}
                 actions={[
                     actions.setBold,
-                    actions.setItalic,
                     actions.insertBulletsList,
                     actions.insertOrderedList,
                     actions.insertImage,
